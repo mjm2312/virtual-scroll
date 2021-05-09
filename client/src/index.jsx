eabd2e9a3e83af72c772f6be67dfcc7328ef7aba
Matt Martin <mjm2312@columbia.edu>
2021-05-09 19:09:39 -0400

    Initial commit. Sending CSV data from server and displaying it in table. Working on virtual scrolling with css/target el properties, will try to implement with refs/intersectionObserver

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,17 +1,102 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import Papa from "papaparse";
+
 
 class App extends React.Component {
   constructor() {
     super();
     this.state = {
-      someState: "value of state"
+      someState: "value of state",
+      data: [],
+      xOffset: 1
     };
+    this.viewportElement = React.createRef()
+    this.lastVisCol = React.createRef() //try with intersectionObserver
+
+    this.handleScroll = this.handleScroll.bind(this);
+    this.printTable = this.printTable.bind(this);
+  }
+
+  componentDidMount() {
+    const context = this;
+    Papa.parse('data.csv', {
+      header: true,
+      download: true,
+      dynamicTyping: true,
+      complete: function(results) {
+        //data = results.data;
+        context.setState({
+          data: results.data
+        })
+      }
+    });
+  }
+
+  handleScroll(el) {
+    //console.log(el.target.scrollTop)
+    console.log(el.target.scrollLeft)
+    if (el.target.scrollLeft >= 640) {
+      this.setState({
+        xOffset: this.state.xOffset += 1
+      })
+      console.log('ref dot curr, ', this.viewportElement.current)
+      console.log('vs')
+      console.log('el.target', el.target);
+      this.viewportElement.current.scrollLeft = 0;
+    }
+  }
+
+  printTable() {
+    return (
+    //header 
+    <table id = "table"> 
+      <thead>
+        <tr>
+          {Object.keys(this.state.data[0]).map((key, i) => {
+            if (i >= (this.state.xOffset - 1) * 20 && i < this.state.xOffset * 20) {
+              return (<th>{key}</th>)
+            }
+          })} 
+        </tr>
+      </thead>
+      
+      <tbody> 
+      {/* body */}
+        {this.state.data.map((row, index) => {
+        if (index < 10) {
+          return (
+            <tr>
+            {Object.keys(row).map((key, i) => {
+              if (i < 20) {
+                return (<td> {row[key]} </td>)
+              }
+            })
+            }
+          </tr>
+          )
+        }
+        })}
+      </tbody>
+    </table>
+  )
   }
 
   render() {
-    return <div>Hello! Does this work?</div>;
-  }
+    if (this.state.data.length === 0) {
+      return (<div>loading table
+      </div>);
+    } else {
+      return (
+        //  <div> not undefined </div>
+        <div ref = {this.viewportElement}
+             id = "table-container" 
+             onScroll = {(el) => this.handleScroll(el)}> 
+          {this.printTable()}
+        </div>
+      )
+    }
+  };
 }
 
 ReactDOM.render(<App />, document.getElementById("app"));
--- server/index.js
+++ server/index.js
@@ -6,8 +6,21 @@ const app = express();
 app.use(bodyParser.json());
 app.use(express.static(__dirname + "/../client/dist"));
 
-app.get("/route", (req, res) => {
-  res.status(200).json("Server is working!");
+app.get("/", (req, res) => {
+  //res.status(200).json("Server is working!");
+  res.json("in the / route")
+});
+
+app.get("/data.csv", (req, res) => {
+  res.sendFile(__dirname + '/data.csv');
+})
+
+app.get("/test", (req, res) => {
+  //res.status(200).json("Server is working!");
+  
+  console.log('form hit test route ')
+  //res.json("in the test route")
+  res.redirect('/');
 });
 
 app.listen(port, () => {
