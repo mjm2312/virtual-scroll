c6ecc8b48576951dc622c2823ab2b64b4342ae06
Matt Martin <mjm2312@columbia.edu>
2021-05-12 16:58:02 -0400

    linted main files

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,193 +1,85 @@
-//import React from "react";
-import ReactDOM from "react-dom";
-import Papa from "papaparse";
-//import mockData from './mockData';
-
-//console.log('what is mockdata', mockData)
-
-
-
-import React, { useState, useRef, useCallback, useEffect } from 'react'
-import printTable from './printTable.js'
+import ReactDOM from 'react-dom';
+import Papa from 'papaparse';
+import React, {
+  useState, useRef, useCallback, useEffect,
+} from 'react';
+import printTable from './printTable.js';
 
 export default function App() {
-  const [query, setQuery] = useState('')
-  const [pageNumber, setPageNumber] = useState(1)
   const [data, setData] = useState([]);
   const [xOffset, setxOffset] = useState(1);
-  const [loading, setLoading] = useState(true)
-
-  const [x, setX] = useState(0);
-
-  //const [scrollsLeft, setScrollsLeft] = useState(false);
-  //const [scrollsRight, setScrollsRight] = useState(true);
-  const [scrollDir, setScrollDir] = useState('right');
-
+  const [loading, setLoading] = useState(true);
   const viewportElement = useRef();
 
-
-  const handleNavigation = useCallback(
-    (e) => {
-      const container = e.currentTarget;
-      //console.log(x - container.scrollLeft);    
-      if (x > container.scrollLeft && x < 600) {
-        setScrollDir('left');
-      } else if (x < container.scrollLeft) {
-        setScrollDir('right');
-      } else if (x === 0) {
-      }
-      //console.log(container.scrollLeftMax);
-      //console.log(container.scrollLeft);
-      //console.log('width,', container.scrollWidth);
-      //console.log(container.scrollWidth - container.scrollLeft)
-      //console.log(container.scrollWidth - 600);
-      setX(container.scrollLeft);
-    },
-    [x]
-  );
-
-
-
-
-
-    
-  const observer = useRef()
-  const lastVisColRef = useCallback(node => {
-
-    if (loading) return
-    
-    if (observer.current) observer.current.disconnect()
-    observer.current = new IntersectionObserver(entries => {
-      //console.log('entries', entries);
-      //console.log('entries[0]', entries[0]);
-      console.log('entries', entries[0]);
-
-      if (entries[0].isIntersecting) {
-        setxOffset(xOffset => xOffset + 1);
-        //console.log('what is x during intersection', x);
-
-        viewportElement.current.scrollLeft = 0; //need this bc otherwise scrollbar stays in position that continuously reveals next intersectionObeserver ref
-        viewportElement.current.scrollLeft += 200;
-      }
-    })
-    //if (node) observer.current.observe(node)
-    const rows = document.querySelectorAll('.lastVisCol');
-    console.log('what is rows', rows);
-    if (rows) {
-      rows.forEach(row => {
-        observer.current.observe(row);
-      })
-    }
-
-
-
-  }, [loading])
-
-  const observer2 = useRef()
-
-  const firstVisColRef = useCallback(node => {
-    console.log('node', node);
-    //if (loading) return
-    
-    if (observer2.current) observer2.current.disconnect()
-    observer2.current = new IntersectionObserver(entries => {
-      if (entries[0].isIntersecting) {
-      //console.log('what is x during new intersection', x);
-
-        // if (scrollDir ==='left') {
-        //   console.log('LEFT we are intersecting with new while left ref')
-        // }
-
-        // console.log('new ref visible')
-        setxOffset(xOffset => xOffset - 1)
-        viewportElement.current.scrollLeft = 0; //need this bc otherwise scrollbar stays in position that continuously reveals next intersectionObeserver ref
-        viewportElement.current.scrollLeft += 600; //use last scroll position instead of this
-      }
-    })
-    //if (node) observer2.current.observe(node)
-    const rows = document.querySelectorAll('.firstVisCol');
-    console.log('what is first rows', rows);
-    if (rows) {
-      rows.forEach(row => {
-        observer2.current.observe(row);
-      })
-    }
-  }, [loading])
-
-
-  useEffect(() => {  
-    //const context = this;
+  useEffect(() => {
     Papa.parse('data.csv', {
       header: true,
       download: true,
       dynamicTyping: true,
-      complete: function(results) {
-        console.log(results.data);
+      complete(results) {
         setData([...data, ...results.data]);
         setLoading(false);
+      },
+    });
+  }, []);
 
-      }
-    })
-  }, [])
-
-  if (data.length === 0) { //OR BETTER, USE, IF LOADING
-    return (<div> loading data ...</div>)
-  } else {
-    return (
-      <div>
-        <div ref = {viewportElement}
-            id = "table-container"
-            onScroll = {handleNavigation}
-        >
-        {printTable(xOffset, data, lastVisColRef, scrollDir, firstVisColRef)}
-        </div>
-      </div>
-    )
+  const options = {
+    root: viewportElement.current,
+    rootMargin: '0px',
+    threshold: 1.0
   }
+  const observeLastCol = useRef();
+  const lastVisColRef = useCallback(() => {
+    if (loading) return;
+    if (observeLastCol.current) observeLastCol.current.disconnect();
+
+    observeLastCol.current = new IntersectionObserver((entries) => {
+      if (entries[0].isIntersecting) {
+        setxOffset((xOffset) => xOffset + 1);
+        viewportElement.current.scrollLeft = 0;
+        viewportElement.current.scrollLeft += 200; // otherwise, scrollbar stays in position that continuously reveals next intersectionObeserver target el
+      }
+    }, options);
+
+    const rows = document.querySelectorAll('.lastVisCol');
+    rows.forEach((row) => {
+      observeLastCol.current.observe(row);
+    });
+  }, [loading]);
+
+  const observeFirstCol = useRef();
+  const firstVisColRef = useCallback(() => {
+    if (observeFirstCol.current) observeFirstCol.current.disconnect();
+
+    observeFirstCol.current = new IntersectionObserver((entries) => {
+      if (entries[0].isIntersecting) {
+        setxOffset((xOffset) => xOffset - 1);
+        viewportElement.current.scrollLeft = 0; // need this bc otherwise scrollbar stays in position that continuously reveals next intersectionObeserver ref
+        viewportElement.current.scrollLeft += 1800; // use last scroll position instead of this
+      }
+    });
+
+    const rows = document.querySelectorAll('.firstVisCol');
+    rows.forEach((row) => {
+      observeFirstCol.current.observe(row);
+    });
+  }, [loading]);
+
+  if (loading) {
+    return (
+      <div id="table-container"> loading data...</div>
+    );
+  }
+  return (
+    <div>
+      <div
+        ref={viewportElement}
+        id="table-container"
+      >
+        {printTable(xOffset, data, lastVisColRef, firstVisColRef)}
+      </div>
+    </div>
+  );
 }
 
-ReactDOM.render(<App />, document.getElementById('app'))
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+ReactDOM.render(<App />, document.getElementById('app'));
--- client/src/printTable.js
+++ client/src/printTable.js
@@ -1,186 +1,92 @@
-import React from "react";
+import React from 'react';
 
-const NUM = 20; //size of batch;
+const NUM = 20; // size of batch;
 
-//NEED TO APPLY SENTINEL LOGIC TO WHOLE COLUMN NOT JUST TD HEADER
+export default function printTable(xOffset, data, lastVisColRef, firstVisColRef) {
+  return (
+  // header
+    <div id="table-wrapper">
+      <table>
+        <thead>
+          <tr>
+            {Object.keys(data[0]).map((key, i) => {
+              if (i >= (xOffset - 1) * NUM && i <= (xOffset * NUM) - 1) {
+                return (<th>{key}</th>);
+              } if (i === (xOffset * NUM)) {
+                return (
+                  <th ref={lastVisColRef}>
+                    {' '}
+                    RS
+                    {key}
+                  </th>
+                ); // need to adapt to this sol
+              } if (i === ((xOffset - 1) * NUM) - 1) {
+                return (
+                  <th>
+                    {' '}
+                    {key}
+                  </th>
+                ); // scrolling ltr, this is last value we saw, e.g. 20. repeat it again here for rtl scrolls.
+              } if (i === ((xOffset - 1) * NUM) - 2) {
+                return (
+                  <th ref={firstVisColRef}>
+                    {' '}
+                    hi left
+                    {key}
+                  </th>
+                ); // 1 value prior to last val (e.g. 19), so use it as sentinel
+              }
+            })}
+          </tr>
+        </thead>
 
-export default function printTable(xOffset, data, lastVisColRef, scrollDir, firstVisColRef) {
-  scrollDir = 'right';
-  
-  if (scrollDir === 'right') { 
-    return (
-      //header 
-      <div id = "table wrapper">
-        <table id = "table1"> 
-          <thead>
-            <tr>
-              {Object.keys(data[0]).map((key, i) => {
-                if (i >= (xOffset - 1) * NUM && i <= (xOffset * NUM) - 1) {
-                  return (<th>{key}</th>)
-                } else if (i === (xOffset * NUM)) {
-                  return (<th ref = {lastVisColRef}> RS {key}</th>) //need to adapt to this sol
-                } 
-                //else if (i === 0 && xOffset === 1) {
-                //   return (<th> not a sentinel {key}</th>)
-                // } 
-                // else if (i === (xOffset - 1) * NUM) {
-                //   return (<th> LS {key}</th>) //need to adapt to this sol
-                // } 
-                else if (i === ((xOffset - 1) * NUM) - 1) {
-                  return (<th> hi {key}</th>) //including to
-                } else if (i === ((xOffset - 1) * NUM) - 2) {
-                  return (<th ref = {firstVisColRef}> hi left {key}</th>) //need to adapt to this sol
-                }
-      
-    
-              })} 
-            </tr>
-          </thead>
-          
-          <tbody> 
+        <tbody>
           {/* body */}
-            {data.map((row, index) => {
-            if (index < 50) { //only need first 50 for assignment
+          {data.map((row, index) => {
+            if (index < 50) { // only need first 50 for assignment
               return (
                 <tr>
                   {Object.keys(row).map((key, i) => {
-                    if (i > (xOffset - 1) * NUM && i !== (xOffset * NUM)   && i <= (xOffset * NUM) ) { //ADD +3 to end this is just for adjacent table testing. Correct this + 3 thing. lookinto why you're rendering more cols of th than td on the right side
-                      return row[key] === null ? <td className = "null-cell"> null </td> : <td> {row[key]} </td>
-                    } else if (i === (xOffset * NUM)) {
-                      return <td className = "lastVisCol"> {row[key]} </td>
-                    } else if (i === ((xOffset - 1) * NUM) - 2) {
-                      return <td className = "firstVisCol"> {row[key]} </td>
+                    if (i > (xOffset - 1) * NUM && i !== (xOffset * NUM) && i <= (xOffset * NUM)) { // +2 bc we render 2 extra headers (last two else ifs)
+                      return row[key] === null ? <td className="null-cell"> null </td> : (
+                        <td>
+                            {' '}
+                            {row[key]}
+                            {' '}
+                          </td>
+                      );
+                    } if (i === (xOffset * NUM)) {
+                      return row[key] === null ? <td className="null-cell lastVisCol"> null </td> : (
+                        <td className="lastVisCol">
+                            {' '}
+                            {row[key]}
+                            {' '}
+                          </td>
+                      );
+                    } if (i === ((xOffset - 1) * NUM) - 1) {
+                      return row[key] === null ? <td className="null-cell"> null </td> : (
+                        <td>
+                            {' '}
+                            {row[key]}
+                            {' '}
+                          </td>
+                      );
+                    } if (i === ((xOffset - 1) * NUM) - 2) {
+                      return row[key] === null ? <td className="null-cell firstVisCol"> null </td> : (
+                        <td className="firstVisCol">
+                            {' '}
+                            {row[key]}
+                            {' '}
+                          </td>
+                      );
                     }
                   })}
                 </tr>
-              )
-              }
-
-            })}
-          </tbody>
-        </table>
-      
-       {/*dup */}
-        {/* <table id = "table2"> 
-          <thead>
-            <tr>
-              {Object.keys(data[0]).map((key, i) => {
-                if (i >= (xOffset - 1) * NUM && i <= (xOffset * NUM) - 1) {
-                  return (<th>{key}</th>)
-                } else if (i === (xOffset * NUM)) {
-                  return (<th ref = {lastVisColRef}> RS {key}</th>) //need to adapt to this sol
-                } 
-                //else if (i === 0 && xOffset === 1) {
-                //   return (<th> not a sentinel {key}</th>)
-                // } 
-                // else if (i === (xOffset - 1) * NUM) {
-                //   return (<th> LS {key}</th>) //need to adapt to this sol
-                // } 
-                else if (i === ((xOffset - 1) * NUM) - 1) {
-                  return (<th> hi {key}</th>) //including to
-                } else if (i === ((xOffset - 1) * NUM) - 2) {
-                  return (<th ref = {firstVisColRef}> hi left {key}</th>) //need to adapt to this sol
-                }
-      
-    
-              })} 
-            </tr>
-          </thead>
-          
-          <tbody> 
-          {/* body */}
-            {/* {data.map((row, index) => {
-            if (index < 50) { //only need first 50 for assignment
-              return (
-                <tr>
-                  {Object.keys(row).map((key, i) => {
-                    if (i > (xOffset - 1) * NUM && i <= (xOffset * NUM) + 3) { //Correct this + 3 thing. lookinto why you're rendering more cols of th than td on the right side
-                      return row[key] === null ? <td className = "null-cell"> null </td> : <td> {row[key]} </td>
-                    } 
-                  })}
-                </tr>
-              )
-              }
-
-            })}
-          </tbody>
-        </table> */} */}
-
-
-
-
-
-
-
-
-
-
-
-      </div>
-    )
-  
-  
-  
-  
-  
-  
-  //Need to delete this
-  } else if (scrollDir === 'left') {
-    return (
-      //header 
-      <div id = "table wrapper">
-        <table id = "table"> 
-          <thead>
-            <tr>
-              {Object.keys(data[0]).map((key, i) => {
-                if (i >= (xOffset - 2) * 20 && i < (xOffset - 1 * 20) - 1) {
-                  return (<th>{key}</th>)
-                } else if (i === (xOffset - 1 * 20)) {
-                  return (<th ref = {firstVisColRef}> hi {key}</th>) //need to adapt to this sol
-                } 
-      
-    
-              })} 
-            </tr>
-          </thead>
-          
-          <tbody> 
-          {/* body */}
-            {data.map((row, index) => {
-            if (index < 20) {
-              return (
-                <tr>
-                {Object.keys(row).map((key, i) => {
-                  if (i < 20) {
-                    console.log(row[key] === null)
-                    return row[key] === null ? <td> null </td> : <td> {row[key]} </td>
-                  }
-                })
-                }
-              </tr>
-              )
+              );
             }
-            })}
-          </tbody>
-        </table>
-      </div>
-    )
-  } else {
-    return (<div> what scrollDIr {scrollDir} </div>)
-  }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+          })}
+        </tbody>
+      </table>
+    </div>
+  );
 }
